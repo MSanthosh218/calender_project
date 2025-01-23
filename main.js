fb871d12354bc5d9e03c1d9ff5fef2bfa4343a8f
M.Santosh <santhoshmsanthosh218@gmail.com>
2025-01-23 12:31:06 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// document.addEventListener("DOMContentLoaded",function(){
+//     const months=["jan","feb","mar","apr","may",
+//         "june","july","August","sep","oct",
+//         "nov" ,"dec"
+//     ]
+//     const today=new Date()
+//     const currmonthsno=today.getMonth()
+//     document.getElementsById("curr-month").textContent = months[currmonthsno];
+//     document.getElementById("full-year").textContent = today.toDateString();
+//     // const firstdayofthemonth = new Date(today.getFullYear(),today.getMonth(),1).getday
+//     // console.log(firstdayofthemonth)
+// })
+document.addEventListener("DOMContentLoaded",function(){
+    const months=["jan","feb","mar","apr","may",
+        "june","july","August","sep","oct",
+        "nov" ,"dec"
+    ]
+    const today=new Date()
+    console.log(today)
+})
